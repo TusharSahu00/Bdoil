6563f03366fc816661bc2fb06014379180d2464c
TusharSahu00 <115157644+TusharSahu00@users.noreply.github.com>
2022-11-21 17:13:35 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 20) {
+        $('#toTopBtn').fadeIn();
+      } else {
+        $('#toTopBtn').fadeOut();
+      }
+    });
+  
+    $('#toTopBtn').click(function() {
+      $("html, body").animate({
+        scrollTop: 0
+      }, 1000);
+      return false;
+    });
+  });
+
+  $(window).scroll(function() {
+    $('nav').toggleClass('scrolled',$(this).scrollTop()>50); 
+  });
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+
+
+
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+let masks = document.querySelectorAll('.mask');
+
+masks.forEach( mask => {
+    let image = mask.querySelector('img');
+
+    let tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: mask,
+            toggleActions: "restart none none reset"
+        }
+    });
+
+    tl.set(mask, {autoAlpha: 1});
+
+    tl.from(mask, 1.5, {
+        xPercent: -100,
+        ease: Power2.out
+    });
+    tl.from(image, 1.5, {
+      xPercent: 100,
+      scale: 1.3,
+      delay: -1.5,
+      ease: Power2.out
+  });
+})
+
